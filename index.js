6d9998b00cedb84a1bbe84876e3328279dc91c1b
Abdalla Jama <abdallajama201@gmail.com>
2022-06-20 15:39:44 -0400

    scema and seed data added

--- index.js
+++ index.js
@@ -2,3 +2,12 @@ const inquirer = require("inquirer");
 const mysql = require("mysql2");
 const consoleTable = require("console.table");
 
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      user: 'root',
+      password: 'password',
+      database: 'employee_tracker'
+    },
+    console.log(`Connected to the employee_tracker database.`)
+);
